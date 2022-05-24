bdcecb3407fad52119d5b39b02b1176bc0ebc68f
Esra Alhussaini <alhussaini.esra@gmail.com>
2022-05-24 18:51:22 +0300

    Initial Commit

---
+++ sayar/sayarApp.swift
@@ -0,0 +1,17 @@
+//
+//  sayarApp.swift
+//  sayar
+//
+//  Created by ESRA ALHUSSAINI on 24/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct sayarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
