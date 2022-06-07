b92ae30427d0c399a7bf94addc1f79ad78c206e3
RazanAlta <65066845+RazanAlta@users.noreply.github.com>
2022-06-07 11:08:36 +0300

    Second commit

---
+++ sayar/Views/DashBoard/AddFuel View.swift
@@ -0,0 +1,30 @@
+//
+//  AddFuel View.swift
+//  sayar
+//
+//  Created by Razan Altamimi on 08/11/1443 AH.
+//
+
+import SwiftUI
+
+struct AddFuel_View: View {
+    
+    var body: some View {
+        
+        VStack{
+           
+
+            
+            
+            
+        }
+        .background(Color("Grey"))
+    }
+}
+
+struct AddFuel_View_Previews: PreviewProvider {
+    static var previews: some View {
+        AddFuel_View()
+            .preferredColorScheme(.light)
+    }
+}
