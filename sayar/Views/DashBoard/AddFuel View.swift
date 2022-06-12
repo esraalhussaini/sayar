0f48c30840f79df79b6bbdeece08b2da23b17f7a
RazanAlta <65066845+RazanAlta@users.noreply.github.com>
2022-06-12 13:25:23 +0300

    Add fuel

--- sayar/Views/DashBoard/AddFuel View.swift
+++ sayar/Views/DashBoard/AddFuel View.swift
@@ -9,17 +9,64 @@ import SwiftUI
 
 struct AddFuel_View: View {
     
+    @State private var SAR = ""
+    @State private var KM = ""
+    @State private var DATE = ""
+    
     var body: some View {
-       
-      
         
-        VStack{
-           
-            Text("لازم نص لان طالع ايرور مافيه كود حطيته بشكل مؤقت")
+        NavigationView{
+        Form
+        {
             
-            
+            Section{
+                
+                TextField("13/4/2022",text: $DATE)
+                  
+                
+               
+                
+                
+            }
+            Section{
+                TextField("SAR",text: $SAR)
+                TextField("KM",text: $KM)
+                
+            }
             
         }
+        .toolbar{
+            ToolbarItemGroup(placement: .navigationBarLeading){
+                
+                
+                Button("Cancel"){
+                    
+                    print("Cancelled")
+                    
+                }
+                Button("Done"){
+                    
+                    print("Has been saved")
+                }
+               
+                
+            }
+           
+           
+            
+        }
+            
+        }.accentColor(.red)
+       
+      
+//
+//        VStack{
+//
+//            Text("لازم نص لان طالع ايرور مافيه كود حطيته بشكل مؤقت")
+//
+//
+//
+//        }
         .background(Color("Grey"))
     }
 }
