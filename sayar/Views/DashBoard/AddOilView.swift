563c9fde99ddb1fabad7181c61e706a84eec1e9f
RazanAlta <65066845+RazanAlta@users.noreply.github.com>
2022-06-12 14:04:02 +0300

    Add Oil

---
+++ sayar/Views/DashBoard/AddOilView.swift
@@ -0,0 +1,81 @@
+//
+//  AddOilView.swift
+//  sayar
+//
+//  Created by Razan Altamimi on 13/11/1443 AH.
+//
+
+import SwiftUI
+
+struct AddOilView: View {
+    
+    @State private var SAR = ""
+    @State private var PickKM = ""
+    @State private var DATE = ""
+    
+    var body: some View {
+   
+        
+        NavigationView{
+        Form
+        {
+            
+            Section{
+                
+                TextField("13/4/2022",text: $DATE)
+                  
+                
+               
+                
+                
+            }
+            Section{
+                TextField("SAR",text: $SAR)
+                TextField("KM",text: $PickKM)
+                
+            }
+            
+        }
+        .toolbar{
+            ToolbarItemGroup(placement: .navigationBarLeading){
+                
+                
+                Button("Cancel"){
+                    
+                    print("Cancelled")
+                    
+                }
+                Button("Done"){
+                    
+                    print("Has been saved")
+                }
+               
+                
+            }
+           
+           
+            
+        }
+            
+        }.accentColor(.red)
+       
+      
+//
+//        VStack{
+//
+//            Text("لازم نص لان طالع ايرور مافيه كود حطيته بشكل مؤقت")
+//
+//
+//
+//        }
+        .background(Color("Grey"))
+    }
+        
+    }
+
+
+struct AddOilView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddOilView()
+    }
+}
