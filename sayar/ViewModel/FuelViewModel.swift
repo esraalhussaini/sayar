fb90242cf201491d30c3a1b142d408f3287e70ca
AnwarAlshammari <Anwar.oc@outlook.sa>
2022-06-21 15:37:15 +0300

    comment for a page Razan

--- sayar/ViewModel/FuelViewModel.swift
+++ sayar/ViewModel/FuelViewModel.swift
@@ -11,34 +11,34 @@ import Firebase
 //import FirebaseCore
 import FirebaseFirestore
 
-class ViewModel: ObservableObject{
-    
-    @Published var list = [Fuel]()
-    
-    func getData(){
-        
-        //get a refrence to the database
-        let db = Firestore.firestore()
-        
-        // read the documents at a specific path
-        db.collection("Fuel").getDocuments { snapshot , error in
-             //check for error
-            if error == nil{
-                //no errors
-            }
-            
-            if let snapshot = snapshot {
-                
-                //get all the document and create instance of fuel
-                snapshot.documents.map { d in
-                  return Fuel(data: <#T##[String : Any]#>)                }
-                
-                
-                
-            }else{}
-        }
-        
-        
-    }
-    
-}
+//class ViewModel: ObservableObject{
+//
+//    @Published var list = [Fuel]()
+//
+//    func getData(){
+//
+//        //get a refrence to the database
+//        let db = Firestore.firestore()
+//
+//        // read the documents at a specific path
+//        db.collection("Fuel").getDocuments { snapshot , error in
+//             //check for error
+//            if error == nil{
+//                //no errors
+//            }
+//
+//            if let snapshot = snapshot {
+//
+//                //get all the document and create instance of fuel
+//                snapshot.documents.map { d in
+//                  return Fuel(data: <#T##[String : Any]#>)                }
+//
+//
+//
+//            }else{}
+//        }
+//
+//
+//    }
+//
+//}
